c2238d1038de500329c256d417d5e81307278731
Duduf <git@duduf.com>
2021-05-03 19:59:17 +0200

    updated icons & added incremental save

--- devFile.py
+++ devFile.py
@@ -9,12 +9,8 @@ import ramses as ram
 ramses = ram.Ramses.instance()
 settings = ram.RamSettings.instance()
 
-def ramSave():
-    # The current maya file
-    currentFilePath = cmds.file( q=True, sn=True )
-    ram.log("Saving file: " + currentFilePath)
-       
-    # Check if the Daemon is available if Ramses is set to be used "online"
+def checkDaemon():
+    """Checks if the Daemon is available (if the settings tell we have to work with it)"""
     if settings.online:
         if not ramses.connect():
             cmds.confirmDialog(
@@ -25,33 +21,46 @@ def ramSave():
                 )
             ramses.showClient()
             cmds.error( "User not available: You must log in Ramses first!" )
-            return
+            return False
 
-    # Get the save path (Ramses will check if the current file has to be renamed to respect the Ramses Tree and Naming Scheme)
-    saveFilePath = ram.RamFileManager.getSaveFilePath( currentFilePath )
+    return True
+
+def getSaveFilePath( filePath ):
+    # Ramses will check if the current file has to be renamed to respect the Ramses Tree and Naming Scheme
+    saveFilePath = ram.RamFileManager.getSaveFilePath( filePath )
     if not saveFilePath: # Ramses may return None if the current file name does not respeect the Ramses Naming Scheme
         cmds.warning( ram.Log.MalformedName )
         # Set file to be renamed
         cmds.file( renameToSave = True )
         cmds.inViewMessage( msg='Malformed Ramses file name! <hl>Please save with a correct name first</hl>.', pos='midCenter', fade=True )
+        return None
+
+    return saveFilePath
+
+    
+def ramSaveIncremental():
+    # The current maya file
+    currentFilePath = cmds.file( q=True, sn=True )
+    ram.log("Saving file: " + currentFilePath)
+       
+    # Check if the Daemon is available if Ramses is set to be used "online"
+    if not checkDaemon():
         return
 
-    # If the current Maya file is inside a preview/publish/version subfolder, we're going to increment
-    # to be sure to not lose the previous working file.
-    increment = False
-    if ram.RamFileManager.inReservedFolder( currentFilePath ):
-        increment = True
-        cmds.warning( "Incremented and Saved as " + saveFilePath )
+    # Get the save path 
+    saveFilePath = getSaveFilePath( currentFilePath )
+    if not saveFilePath:
+        return
 
     # Set the save name and save
     cmds.file( rename = saveFilePath )
     cmds.file( save=True, options="v=1;" )
     # Backup / Increment
-    backupFilePath = ram.RamFileManager.copyToVersion( saveFilePath, increment=increment )
+    backupFilePath = ram.RamFileManager.copyToVersion( saveFilePath, increment=True )
     backupFileName = os.path.basename( backupFilePath )
     decomposedFileName = ram.RamFileManager.decomposeRamsesFileName( backupFileName )
     newVersion = str( decomposedFileName['version'] )
-    ram.log( "Scene saved! Current version is: " + newVersion )
-    cmds.inViewMessage( msg='Scene saved! <hl>v' + newVersion + '</hl>', pos='midCenter', fade=True )
+    ram.log( "Incremental save, scene saved! New version is: " + newVersion )
+    cmds.inViewMessage( msg='Incremental save! New version: <hl>v' + newVersion + '</hl>', pos='midCenter', fade=True )
     
-ramSave()
+ramSaveIncremental()
--- plug-in/ramses_maya/ram_cmds.py
+++ plug-in/ramses_maya/ram_cmds.py
@@ -16,6 +16,34 @@ import ramses as ram
 ramses = ram.Ramses.instance()
 settings = ram.RamSettings.instance()
 
+def checkDaemon():
+    """Checks if the Daemon is available (if the settings tell we have to work with it)"""
+    if settings.online:
+        if not ramses.connect():
+            cmds.confirmDialog(
+                title="No User",
+                message="You must log in Ramses first!",
+                button=["OK"],
+                icon="warning"
+                )
+            ramses.showClient()
+            cmds.error( "User not available: You must log in Ramses first!" )
+            return False
+
+    return True
+
+def getSaveFilePath( filePath ):
+    # Ramses will check if the current file has to be renamed to respect the Ramses Tree and Naming Scheme
+    saveFilePath = ram.RamFileManager.getSaveFilePath( filePath )
+    if not saveFilePath: # Ramses may return None if the current file name does not respeect the Ramses Naming Scheme
+        cmds.warning( ram.Log.MalformedName )
+        # Set file to be renamed
+        cmds.file( renameToSave = True )
+        cmds.inViewMessage( msg='Malformed Ramses file name! <hl>Please save with a correct name first</hl>.', pos='midCenter', fade=True )
+        return None
+
+    return saveFilePath
+
 class RamOpenCmd( om.MPxCommand ):
     name = "ramOpen"
 
@@ -47,25 +75,12 @@ class RamSaveCmd( om.MPxCommand ):
         ram.log("Saving file: " + currentFilePath)
         
         # Check if the Daemon is available if Ramses is set to be used "online"
-        if settings.online:
-            if not ramses.connect():
-                cmds.confirmDialog(
-                    title="No User",
-                    message="You must log in Ramses first!",
-                    button=["OK"],
-                    icon="warning"
-                    )
-                ramses.showClient()
-                cmds.error( "User not available: You must log in Ramses first!" )
-                return
+        if not checkDaemon():
+            return
 
-        # Get the save path (Ramses will check if the current file has to be renamed to respect the Ramses Tree and Naming Scheme)
-        saveFilePath = ram.RamFileManager.getSaveFilePath( currentFilePath )
-        if not saveFilePath: # Ramses may return None if the current file name does not respeect the Ramses Naming Scheme
-            cmds.warning( ram.Log.MalformedName )
-            # Set file to be renamed
-            cmds.file( renameToSave = True )
-            cmds.inViewMessage( msg='Malformed Ramses file name! <hl>Please save with a correct name first</hl>.', pos='midCenter', fade=True )
+        # Get the save path 
+        saveFilePath = getSaveFilePath( currentFilePath )
+        if not saveFilePath:
             return
 
         # If the current Maya file is inside a preview/publish/version subfolder, we're going to increment
@@ -97,7 +112,29 @@ class RamSaveVersionCmd( om.MPxCommand ):
         return RamSaveVersionCmd()
 
     def doIt(self, args):
-        ram.log("Command 'save new version' is not implemented yet!")
+        # The current maya file
+        currentFilePath = cmds.file( q=True, sn=True )
+        ram.log("Saving file: " + currentFilePath)
+        
+        # Check if the Daemon is available if Ramses is set to be used "online"
+        if not checkDaemon():
+            return
+
+        # Get the save path 
+        saveFilePath = getSaveFilePath( currentFilePath )
+        if not saveFilePath:
+            return
+
+        # Set the save name and save
+        cmds.file( rename = saveFilePath )
+        cmds.file( save=True, options="v=1;" )
+        # Backup / Increment
+        backupFilePath = ram.RamFileManager.copyToVersion( saveFilePath, increment=True )
+        backupFileName = os.path.basename( backupFilePath )
+        decomposedFileName = ram.RamFileManager.decomposeRamsesFileName( backupFileName )
+        newVersion = str( decomposedFileName['version'] )
+        ram.log( "Incremental save, scene saved! New version is: " + newVersion )
+        cmds.inViewMessage( msg='Incremental save! New version: <hl>v' + newVersion + '</hl>', pos='midCenter', fade=True )
 
 class RamPublishCmd( om.MPxCommand ):
     name = "ramPublish"
