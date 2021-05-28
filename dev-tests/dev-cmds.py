d3197768ea833276bbbde66a409790f8a64928c6
Nicolas Dufresne <git@duduf.com>
2021-05-28 12:29:01 +0200

    Update dev-cmds.py

--- dev-tests/dev-cmds.py
+++ dev-tests/dev-cmds.py
@@ -14,8 +14,13 @@ from PySide2.QtWidgets import ( # pylint: disable=no-name-in-module
     QPushButton,
     QLabel,
     QFileDialog,
+    QSlider,
+    QSpinBox,
+    QCheckBox,
+    QTextEdit,
 )
 from PySide2.QtCore import ( # pylint: disable=no-name-in-module
+    Qt,
     QLine,
     Slot,
 )
@@ -24,7 +29,6 @@ import ramses as ram
 
 ramses = ram.Ramses.instance()
 settings = ram.RamSettings.instance()
-
 
 class StateBox( QComboBox ):
     def __init__(self, parent = None):
@@ -341,7 +345,8 @@ def checkDaemon():
 def getSaveFilePath( filePath ):
     # Ramses will check if the current file has to be renamed to respect the Ramses Tree and Naming Scheme
     saveFilePath = ram.RamFileManager.getSaveFilePath( filePath )
-    if not saveFilePath: # Ramses may return None if the current file name does not respeect the Ramses Naming Scheme
+    print(saveFilePath)
+    if saveFilePath == "": # Ramses may return an empty string if the current file name does not respect the Ramses Naming Scheme
         cmds.warning( ram.Log.MalformedName )
         # Set file to be renamed
         cmds.file( renameToSave = True )
@@ -401,6 +406,48 @@ def publishTemplate(): # TODO : template name & Extension as ma or mb
         # Message
         cmds.inViewMessage( msg='Template published as: <hl>' + saveName + '</hl> in ' + saveFolder , pos='midCenter', fade=True )
 
+def save():
+    ram.log("Saving file...")
+
+    # The current maya file
+    currentFilePath = cmds.file( q=True, sn=True )
+    ram.log("Saving file: " + currentFilePath)
+    
+    # We don't need the daemon to just save a file
+    # if not checkDaemon():
+    #     return
+
+    # Get the save path 
+    saveFilePath = getSaveFilePath( currentFilePath )
+    if not saveFilePath:
+        return
+
+    # If the current Maya file is inside a preview/publish/version subfolder, we're going to increment
+    # to be sure to not lose the previous working file.
+    increment = False
+    if ram.RamFileManager.inReservedFolder( currentFilePath ):
+        increment = True
+        cmds.warning( "Incremented and Saved as " + saveFilePath )
+
+    # If the timeout has expired, we're also incrementing
+    prevVersion = ram.RamFileManager.getLatestVersion( saveFilePath, previous=True )
+    modified = prevVersion[2]
+    now = datetime.today()
+    timeout = timedelta(seconds = settings.autoIncrementTimeout * 60 )
+    if  timeout < now - modified:
+        increment = True
+
+    # Set the save name and save
+    cmds.file( rename = saveFilePath )
+    cmds.file( save=True, options="v=1;" )
+    # Backup / Increment
+    backupFilePath = ram.RamFileManager.copyToVersion( saveFilePath, increment=increment )
+    backupFileName = os.path.basename( backupFilePath )
+    decomposedFileName = ram.RamFileManager.decomposeRamsesFileName( backupFileName )
+    newVersion = str( decomposedFileName['version'] )
+    ram.log( "Scene saved! Current version is: " + newVersion )
+    cmds.inViewMessage( msg='Scene saved! <hl>v' + newVersion + '</hl>', pos='midCenter', fade=True )
+
 def saveVersion():
     # The current maya file
     currentFilePath = cmds.file( q=True, sn=True )
@@ -412,7 +459,8 @@ def saveVersion():
 
     # Get the save path 
     saveFilePath = getSaveFilePath( currentFilePath )
-    if saveFilePath == "":
+    print(saveFilePath)
+    if not saveFilePath:
         return
 
     # Update status
@@ -473,3 +521,6 @@ def saveVersion():
     newVersionStr = str( newVersion )
     ram.log( "Incremental save, scene saved! New version is: " + newVersionStr )
     cmds.inViewMessage( msg='Incremental save! New version: <hl>v' + newVersionStr + '</hl>', pos='midCenter', fade=True )
+
+
+saveVersion()
