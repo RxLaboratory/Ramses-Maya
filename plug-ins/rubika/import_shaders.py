ea64d8461d21dbcd026f16c74ab9b273868d473e
Duduf <git@duduf.com>
2021-07-04 10:24:53 +0200

    Revert "Merge branch 'supinfocom-dev' into dev"
    This reverts commit 2c8fb5f02c199513a93ce2519ba9aa0fe5ef4e84, reversing
    changes made to f7f3cdb76ba7049eec3c0cc94116a30a4e2ac23e.

--- plug-ins/rubika/import_shaders.py
+++
@@ -1,5 +0,0 @@
-from .utils_shaders import referenceShaders
-
-def importShaders( item, filePath, mode, nodes=[] ):
-    # Import shaders
-    referenceShaders(nodes, mode, filePath, item.shortName())
