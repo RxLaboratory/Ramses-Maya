4cea6e1c363e6bf638410b265e8f27ba1e0757b0
Duduf <git@duduf.com>
2021-10-20 14:26:42 +0200

    use debugmode

--- plug-ins/rubika/update_manager.py
+++ plug-ins/rubika/update_manager.py
@@ -11,7 +11,14 @@ from .update_anim import updateAnim
 from .utils_attributes import *
 
 def updateRamsesItems():
+    try:
+        runUpdate()
+    except:
+        ram.printException()
+        if ram.RamSettings.instance().debugMode:
+            raise
 
+def runUpdate():
     # We need to check if the scene has been correctly saved first
     # The current maya file
     currentFilePath = cmds.file( q=True, sn=True )
