1e52056b729a2418b3af396a9f8c7fb3e0381c34
Duduf <git@duduf.com>
2022-05-27 12:53:30 +0200

    improved reload module

--- plug-ins/dumaf/mayapy.py
+++ plug-ins/dumaf/mayapy.py
@@ -9,7 +9,7 @@ def reset_script_session(script_path=None):
     if script_path is None:
         script_path = dirname(__file__)
     # Convert this to lower just for a clean comparison later
-    script_path = script_path.lower()
+    script_path = script_path.lower().replace("\\","/")
 
     to_delete = []
     # Iterate over all the modules that are currently loaded
@@ -17,18 +17,19 @@ def reset_script_session(script_path=None):
         # There's a few modules that are going to complain if you try to query them
         # so I've popped this into a try/except to keep it safe
         try:
-        # Use the "inspect" library to get the moduleFilePath that the current module was loaded from
-            module_file_path = inspect.getfile(module).lower()
+            # Use the "inspect" library to get the moduleFilePath that the current module was loaded from
+            module_file_path = inspect.getfile(module).lower().replace("\\","/")
 
             # Don't try and remove the startup script, that will break everything
             if module_file_path == __file__.lower():
                 continue
 
             # If the module's filepath contains the userPath, add it to the list of modules to delete
-            if module_file_path.startswith(script_path):
+            if script_path in module_file_path:
                 print( "Removing %s" % key )
                 to_delete.append(key)
-        except: # pylint: disable=bare-except
+        except Exception as err: # pylint: disable=broad-except
+            # print(err)
             pass
 
     # If we'd deleted the module in the loop above, it would have changed the size of the dictionary and
