bca796992e14836a772badd1d8cda4b76e78f54b
Duduf <git@duduf.com>
2023-01-18 19:28:45 +0100

    bump version to 0.8.4

--- plug-ins/ramses_maya/__init__.py
+++ plug-ins/ramses_maya/__init__.py
@@ -14,7 +14,7 @@ from . import ui_import
 from . import ui_scene_setup
 
 VENDOR = "RxLaboratory"
-VERSION = "0.8.3-Beta"
+VERSION = "0.8.4-Beta"
 TOOL_NAME = "Ramses-Maya"
 IS_PRERELEASE = False
 RAMSES = Ramses.instance()
