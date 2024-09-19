196c3ec5a81c8e65d6d46eba1462359a14ecc390
Duduf <git@duduf.com>
2024-09-19 16:36:55 +0200

    Refactor build script (simplified)

---
+++ tools/deploy.py
@@ -0,0 +1,57 @@
+import os
+import zipfile
+from rxbuilder.utils import (
+    wipe,
+    replace_in_file,
+    zip_dir
+)
+from rxbuilder.py import (
+    build_folder
+)
+from config import (
+    BUILD_PATH,
+    SRC_PATH,
+    VERSION
+)
+
+MOD_PATH = os.path.join( BUILD_PATH, 'ramses-maya' )
+
+def build():
+    if not os.path.isdir(MOD_PATH):
+        os.makedirs(MOD_PATH)
+
+    build_folder(
+        SRC_PATH,
+        MOD_PATH
+    )
+
+    # Update metadata
+    mod_file = os.path.join(
+        MOD_PATH,
+        'Ramses.mod'
+    )
+
+    replace_in_file( {
+        '#version#': VERSION,
+        '#modpath#': 'D:\\Path\\To\\Ramses-Maya'
+    }, mod_file)
+
+    config_file = os.path.join(
+        MOD_PATH, 'plug-ins', 'ramses_maya', 'constants.py'
+    )
+    replace_in_file( {
+        '#version#': VERSION,
+    }, config_file)
+
+def zip_module():
+    zip_file = os.path.join(
+        BUILD_PATH,
+        'ramses-maya_' + VERSION + '.zip'
+    )
+    with zipfile.ZipFile(zip_file, 'w', zipfile.ZIP_DEFLATED) as z:
+        zip_dir(MOD_PATH, z)
+
+if __name__ == '__main__':
+    wipe(BUILD_PATH)
+    build()
+    zip_module()
--- tools/deploy_ramses-maya.py
+++
@@ -1,7 +0,0 @@
-import _setup_env as ramaya
-ramaya.init()
-B = ramaya.builder
-
-if __name__ == '__main__':
-    B.build_py("")
-    B.deploy_py("")
